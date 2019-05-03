ce40a96a24a92a6d180be909ac385eda86b49b96
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-05-03 11:28:03 -0400

    re intalled all dependencies

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express'); 
+const helmet= require("helmet");
+const morgan = require("morgan");
+const actionRouter = require("./data/routers/actions");
+const projectRouter = require("./data/routers/projects");
+
+const server = express();
+
+server.use(express.json(), helmet(), morgan('dev'))
+server.use("/api/actions",actionRouter);
+server.use("/api/projects", projectRouter);
+
+
+server.get('/', (req, res, next) => {
+    res.send(`
+      <h2>Hello World</h2>
+      <p>This is my Sprint Challenge for Web Api's</p>
+      `);
+  });
+
+
+
+module.exports = server;
